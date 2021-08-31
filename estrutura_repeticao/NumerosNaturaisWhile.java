864aff6ff8704361bb8b1987be46f6e75cbf6666
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-08-31 11:19:15 -0300

    Estrutura de repetição de números naturais de 0 a 100 - usando While

---
+++ estrutura_repeticao/NumerosNaturaisWhile.java
@@ -0,0 +1,9 @@
+public class NumerosNaturaisWhile {
+    public static void main(String[] args) {
+        int numerosNaturais = 0;
+        while (numerosNaturais <= 100) {    
+        System.out.print(numerosNaturais + " ");  
+        numerosNaturais++;  
+        }
+    }
+}
