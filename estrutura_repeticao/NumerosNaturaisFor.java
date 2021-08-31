634d9cbe43aa1c38016cf954a41deb72397c8233
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2021-08-31 11:18:25 -0300

    Estrutura de repetição de números naturais de 0 a 100 - usando For

---
+++ estrutura_repeticao/NumerosNaturaisFor.java
@@ -0,0 +1,11 @@
+
+public class NumerosNaturaisFor {
+    public static void main(String[] args) {
+        int i;
+        int vetorNaturais[] = new int[101];       
+        for (i = 0; i < vetorNaturais.length; i++) {
+            vetorNaturais[i] = i;
+            System.out.print(vetorNaturais[i] + " ");
+        }
+    }
+}
